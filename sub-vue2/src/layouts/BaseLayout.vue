2aa798da3135f42d99dd6c47187821297297bae9
may <1062993405@qq.com>
2024-04-12 17:29:05 +0800

    init

---
+++ sub-vue2/src/layouts/BaseLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header
+      class="h-16 w-full bg-black/45 text-white flex flex-row items-center"
+    >
+      Vue2
+    </header>
+    <nav class="flex flex-row gap-3">
+      <router-link to="/page1" class="group">
+        <span class="group-[.router-link-active]:text-pink-500">PAGE1</span>
+      </router-link>
+      <router-link to="/page2" class="group">
+        <span class="group-[.router-link-active]:text-pink-500">PAGE2</span>
+      </router-link>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
+<script></script>
+
+<style lang="less" scoped></style>
